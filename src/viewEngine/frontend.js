4626093e53f9711ba13d09149dbbacf8ad9a0b63
Angel Erick Cruz Olivera <leganux@hotmail.com>
2019-04-17 20:40:35 -0500

    Yaaflex - dinamic assets, SEO, goblas, external libriries

--- src/viewEngine/frontend.js
+++ src/viewEngine/frontend.js
@@ -7,8 +7,9 @@ LangGlobal.EN = require('./../config/lang/EN')
 const CheckSession = require('./../auth/checkSession')
 const env = require('./../config/environment.config')
 const cFunctions = require('./../helpers/common.functions')
+const assets = require('./../config/assets_site.config');
 
-
+//var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 
 
 
@@ -21,7 +22,17 @@ router.get('/', CheckSession, async (req, res) => {
                 theme: env.site_theme.toLowerCase(),
                 lang: cFunctions.getUserLang(req),
                 langTexts: LangGlobal[cFunctions.getUserLang(req)],
-                path: RoutesConfig
+                path: RoutesConfig,
+                assets: assets
+            },
+            seo: {
+                title: 'YAAFLEX - Yet another amazing framework by leganux',
+                description: 'YAAFLEX - yet another amazing framework by leganux',
+                image: 'http://cdn.leganux.com/IMG/integrado.png',
+                domain: req.get('host'),
+                url: req.protocol + '://' + req.get('host') + req.originalUrl,
+                tw_posted_by: '@leganux',
+                og_type: 'article',
             }
         });
 });
@@ -31,10 +42,20 @@ router.get('/themePreview', CheckSession, async (req, res) => {
         {
             data: {},
             config: {
-                theme: env.site_theme.toLowerCase(),
+                theme: 'Cosmo',
                 lang: cFunctions.getUserLang(req),
                 langTexts: LangGlobal[cFunctions.getUserLang(req)],
-                path: RoutesConfig
+                path: RoutesConfig,
+                assets: assets
+            },
+            seo: {
+                title: 'YAAFLEX - Yet another amazing framework by leganux',
+                description: 'YAAFLEX - yet another amazing framework by leganux',
+                image: 'http://cdn.leganux.com/IMG/integrado.png',
+                domain: req.get('host'),
+                url: req.protocol + '://' + req.get('host') + req.originalUrl,
+                tw_posted_by: '@leganux',
+                og_type: 'article',
             }
         });
 });
